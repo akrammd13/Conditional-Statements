a422889b95a5e772d981149b2c297f2a26adc9db
akrammd13 <akrammd013@gmail.com>
2025-10-31 18:06:05 +0530

    new added

---
+++ Exercise11.java
@@ -0,0 +1,12 @@
+// Write a Java program to display n terms of natural numbers and their sum.
+
+import java.util.Scanner;
+
+public class Exercise11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input number: ");
+
+        
+    }
+}
